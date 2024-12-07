d7ddf9a8c187c9b274dadd580efc86e7fc0b42bb
Soham Shanbhag <soham.shanbhag@gmail.com>
2024-12-07 17:09:06 +0900

    Solution to Day 7

---
+++ Day7/p2.cpp
@@ -0,0 +1,70 @@
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <print>
+#include <iterator>
+
+using dtype = long long unsigned int;
+// using dtype = unsigned __int128;
+
+std::unordered_map<std::string, dtype> calc(const std::span<int> values, size_t pos, const std::unordered_map<std::string, dtype> memo) {
+    std::unordered_map<std::string, dtype> output = {};
+    for(const auto& [key, val]: memo) {
+        output[key + '+'] = val + values[pos];
+        output[key + '*'] = val * values[pos];
+
+        output[key + '|'] = val * std::pow(10, static_cast<int>(std::log10(values[pos]))+1) + values[pos];
+    }
+
+    return output;
+}
+
+bool is_valid(dtype result, const std::span<int> rhs_values) {
+    std::unordered_map<std::string, dtype> memo {{"", rhs_values[0]}};
+    for(auto i = 1uz; i < rhs_values.size(); ++i) {
+        memo = calc(rhs_values, i, memo);
+        std::erase_if(memo, [&result](const auto& item) {return item.second > result;});
+    }
+    for(auto [key, val]: memo) if(val == result) return true;
+
+
+    return false;
+}
+
+int main(int argc, char** argv) {
+    if(argc != 2) {
+        std::println("Usage: {} <input file>", argv[0]);
+        std::exit(1);
+    }
+
+    std::ifstream input_file {argv[1]};
+
+    dtype sum {};
+    while(true) {
+        std::string input_line {};
+        std::getline(input_file, input_line);
+        if(input_file.eof()) break;
+        // std::print("{} ", input_line);
+
+        size_t col_pos = input_line.find(':');
+        dtype result = std::stoull(input_line.substr(0, col_pos));
+
+        std::string rhs = input_line.substr(col_pos+1);
+        std::stringstream iss(rhs);
+        std::vector<int> rhs_values (( std::istream_iterator<int>( iss ) ), ( std::istream_iterator<int>() ));
+        bool validity = is_valid(result, rhs_values);
+        // std::println("{}", validity);
+        if(validity) sum += result;
+    }
+
+    std::println("{}", sum);
+
+
+
+    return 0;
+
+}
